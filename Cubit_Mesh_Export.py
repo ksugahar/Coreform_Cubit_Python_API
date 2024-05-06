99262203ece13c85072b063bc1f537f9b46ed48f
ksugahar <ksugahar@kindai.ac.jp>
2024-05-06 08:58:58 +0200

    update

--- Cubit_Mesh_Export.py
+++ Cubit_Mesh_Export.py
@@ -341,3 +341,183 @@ def export_3D_gmsh_ver4(cubit, FileName):
 		fid.write('$EndElements\n')
 		fid.close()
 	return cubit
+
+def export_2D_Nastran(cubit, FileName):
+	import datetime
+	formatted_date_time = datetime.datetime.now().strftime("%d-%b-%y at %H:%M:%S")
+
+	fid = open(FileName,'w',encoding='UTF-8')
+	fid.write("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n")
+	fid.write("$\n")
+	fid.write("$                         CUBIT version 2023.8 NX Nastran Translator\n")
+	fid.write("$\n")
+	fid.write("f$            File: {FileName}\n")
+	fid.write(f"$      Time Stamp: {formatted_date_time}\n")
+	fid.write("$\n")
+	fid.write("$\n")
+	fid.write("$                        PLEASE CHECK YOUR MODEL FOR UNITS CONSISTENCY.\n")
+	fid.write("$\n")
+	fid.write("$       It should be noted that load ID's from CUBIT may NOT correspond to Nastran SID's\n")
+	fid.write("$ The SID's for the load and restraint sets start at one and increment by one:i.e.,1,2,3,4...\n")
+	fid.write("$\n")
+	fid.write("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n")
+	fid.write("$\n")
+	fid.write("$\n")
+	fid.write("$ -------------------------\n")
+	fid.write("$ Executive Control Section\n")
+	fid.write("$ -------------------------\n")
+	fid.write("$\n")
+	fid.write("SOL 101\n")
+	fid.write("CEND\n")
+	fid.write("$\n")
+	fid.write("$\n")
+	fid.write("$ --------------------\n")
+	fid.write("$ Case Control Section\n")
+	fid.write("$ --------------------\n")
+	fid.write("$\n")
+	fid.write("ECHO = SORT\n")
+	fid.write("$\n")
+	fid.write("$\n")
+	fid.write("$ Name: Initial\n")
+	fid.write("$\n")
+	fid.write("$\n")
+	fid.write("$ Name: Default Set\n")
+	fid.write("$\n")
+	fid.write("SUBCASE = 1\n")
+	fid.write("$\n")
+	fid.write("LABEL = Default Set\n")
+	fid.write("$\n")
+	fid.write("$ -----------------\n")
+	fid.write("$ Bulk Data Section\n")
+	fid.write("$ -----------------\n")
+	fid.write("$\n")
+	fid.write("BEGIN BULK\n")
+	fid.write("$\n")
+	fid.write("$ Params\n")
+	fid.write("$\n")
+	fid.write("$\n")
+	fid.write("$ Node cards\n")
+	fid.write("$\n")
+	for node_id in range(cubit.get_node_count()+1):
+		if cubit.get_node_exists(node_id):
+			coord = cubit.get_nodal_coordinates(node_id)
+			fid.write(f"GRID    {node_id:<8}{0:<8}{coord[0]:> 7.5f}{coord[1]:> 7.5f}{coord[2]:> 7.5f}\n")
+	fid.write("$\n")
+	fid.write("$ Element cards\n")
+	fid.write("$\n")
+	for block_id in cubit.get_block_id_list():
+		name = cubit.get_exodus_entity_name("block",block_id)
+		fid.write("$\n")
+		fid.write(f"$ Name: {name}\n")
+		fid.write("$\n")
+		surface_list = cubit.get_block_surfaces(block_id)
+		for surface_id in surface_list:
+			tri_list = cubit.get_surface_tris(surface_id)
+			for tri_id in tri_list:
+				node_list = cubit.get_connectivity('tri',tri_id)
+				coord1 = cubit.get_nodal_coordinates(node_list[0])
+				coord2 = cubit.get_nodal_coordinates(node_list[1])
+				coord3 = cubit.get_nodal_coordinates(node_list[2])
+				x1 = coord1[0]
+				y1 = coord1[1]
+				x2 = coord2[0]
+				y2 = coord2[1]
+				x3 = coord3[0]
+				y3 = coord3[1]
+				x21 = x2-x1
+				y21 = y2-y1
+				x31 = x3-x1
+				y31 = y3-y1
+				z_cross = x21*y31-x31*y21
+				if z_cross > 0:
+					fid.write(f"CTRIA3  {tri_id:<8}{block_id:<8}{node_list[0]:<8}{node_list[1]:<8}{node_list[2]:<8}\n")
+				else:
+					fid.write(f"CTRIA3  {tri_id:<8}{block_id:<8}{node_list[0]:<8}{node_list[2]:<8}{node_list[1]:<8}\n")
+	fid.write("$\n")
+	fid.write("$ Property cards\n")
+	fid.write("$\n")
+	
+	for block_id in cubit.get_block_id_list():
+		name = cubit.get_exodus_entity_name("block",block_id)
+		fid.write("$\n")
+		fid.write(f"$ Name: {name}\n")
+		fid.write("$\n")
+		fid.write(f"PSHELL  {block_id:< 8}100     1       \n")
+	fid.write("$\n")
+	fid.write("$ Material cards\n")
+	fid.write("$\n")
+	fid.write("$\n")
+	fid.write("$ Name: Default-Steel\n")
+	fid.write("$\n")
+	fid.write("MAT1*   100             206800          80155.039       0.29            \n")
+	fid.write("*       7e-06           1.2e-05         \n")
+	fid.write("$\n")
+	fid.write("$ Restraint cards\n")
+	fid.write("$\n")
+	fid.write("$\n")
+	fid.write("$ Load cards\n")
+	fid.write("$\n")
+	fid.write("$\n")
+	fid.write("$\n")
+	fid.write("ENDDATA\n")
+	fid.close()
+
+def export_2D_geo_mesh(cubit, FileName):
+
+	import numpy
+	import scipy.io
+
+	N = cubit.get_node_count()
+	M = cubit.get_tri_count()
+
+	nodes = []
+	for node_id in range(N+1):
+		coord = cubit.get_nodal_coordinates(node_id)
+		if cubit.get_node_exists(node_id):
+			nodes.append([coord[0],coord[1]])
+
+	for nodeset_id in cubit.get_nodeset_id_list():
+		name = cubit.get_exodus_entity_name("nodeset",nodeset_id)
+		curve_list = cubit.get_nodeset_curves(nodeset_id)
+		node_list = []
+		for curve_id in curve_list:
+			node_list += cubit.get_curve_nodes(curve_id)
+		nodeset = numpy.array([(name, node_list)], dtype=[('name', 'U20'), ('DBCnodes', 'O')])
+		try:
+			nodesets = append(nodesets,nodeset)
+		except:
+			nodesets = nodeset
+
+	conn_matrix = numpy.zeros((M,3))
+	center_x = numpy.zeros((M))
+	center_y = numpy.zeros((M))
+	block_count = cubit.get_block_count()
+	regions = numpy.rec.array([("", [], [])]*(block_count), dtype=[('name', 'U20'), ('Elements', 'O'), ('Nodes', 'O')])
+
+	for block_id in cubit.get_block_id_list():
+		Elements = []
+		name = cubit.get_exodus_entity_name("block",block_id)
+		surface_list = cubit.get_block_surfaces(block_id)
+		Nodes = []
+		Elements = []
+		for surface_id in surface_list:
+			tri_list = cubit.get_surface_tris(surface_id)
+			Elements	+= tri_list
+			for tri_id in tri_list:
+				x = []
+				y = []
+				node_list = cubit.get_connectivity('tri',tri_id)
+				Nodes += node_list
+				conn_matrix[tri_id-1,:] = node_list
+				for node_id in node_list:
+					coord = cubit.get_nodal_coordinates(node_id)
+					x.append(coord[0])
+					y.append(coord[1])
+				center_x[tri_id-1] = numpy.mean(x)
+				center_y[tri_id-1] = numpy.mean(y)
+		regions[block_id-1][0] = name
+		regions[block_id-1][1] = Elements
+		regions[block_id-1][2] = Nodes
+
+	geo = {'conn_matrix':conn_matrix, 'nodes':nodes, 'M':M, 'N':N, 'nodesets':nodesets , 'center_x':center_x, 'center_y':center_y, 'regions':regions }
+	scipy.io.savemat(FileName, {'geo': geo}, format='5', long_field_names=True)
