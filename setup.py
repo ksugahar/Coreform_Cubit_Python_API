c10e3f62b0b6dbe9a7b6aa401fad41368d18978b
ksugahar <ksugahar@kindai.ac.jp>
2024-07-15 18:35:40 +0200

    update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ AUTHOR_EMAIL = 'ksugahar@gmail.com'
 URL = 'https://github.com/ksugahar/Coreform_Cubit_Python_API'
 LICENSE = 'BSD 3-Clause'
 DOWNLOAD_URL = 'https://github.com/ksugahar/Coreform_Cubit_Python_API'
-VERSION =  '0.1.0'
+VERSION =  '0.1.1'
 PYTHON_REQUIRES = ">=3.10"
 
 INSTALL_REQUIRES = [
