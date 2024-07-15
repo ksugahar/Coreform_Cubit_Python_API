79552e9ca6324c78da247bf8b2c560ba57e52e5a
ksugahar <ksugahar@kindai.ac.jp>
2024-07-15 15:17:06 +0200

    update

---
+++ examples/2024_03_12_demo_file/Cubit2gmsh.py
@@ -0,0 +1,21 @@
+import os, sys
+import numpy as np
+sys.path.append("C:/Program Files/Coreform Cubit 2024.3/bin")
+sys.path.append("../")
+
+import cubit
+cubit.init(['cubit','-nojournal','-batch','nographics','-nogui','-noecho'])
+
+if len(sys.argv) > 1:
+	FileName = sys.argv[1]
+else:
+	FileName = 'model'
+with open(FileName + '.jou','r') as fid:
+	strLines = fid.readlines()
+	for n in range(len(strLines)):
+		cubit.cmd(strLines[n])
+
+from Cubit_Mesh_Export import *
+export_3D_gmsh_ver4(cubit, FileName + '_v4.msh')
+os.system(f"gmsh {FileName + '_v4.msh'}")
+
